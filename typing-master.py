0048902ee36006b211a4bc0b88ccae75eda9a2cf
justsheila678 <smercado2468@gmail.com>
2026-02-25 21:57:44 -0400

    added page, and word list

---
+++ typing-master.py
@@ -0,0 +1,9 @@
+import flet as ft
+import random as rand
+
+def main(page: ft.Page):
+    page.title = "Typing Master"
+
+    words = ["Apocalypse", "Sunsets", "Headphones", "Hoodie", "Moon", "Cycle", "Pecan", "Candle", "Tomato", "Cinnamon", "Bounce", 
+             "Medicine", "Crab", "Project", "Voucher", "Wrist", "Fountain", "Manifesting", "Hamper", "Helicopter", "Prosper", "Lebanese", "Wednesday"]
+    
